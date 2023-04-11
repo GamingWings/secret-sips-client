c515454952dcd925aa84a5d0c0fef00abfc52213
Jennifer Nagle <jennifer.nagle423@gmail.com>
2023-04-11 16:04:59 -0400

    Add websocket and some basic styling

--- secret-sips/src/App.js
+++ secret-sips/src/App.js
@@ -1,24 +1,78 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+// import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import { useMemo, useState, useEffect } from "react";
+import { styled } from "@mui/system";
+
+export const LOCAL_STORAGE_THEME = "Theme";
+
+const HomePage = styled("section")({
+  display: "grid",
+  gridTemplateColumns: "auto",
+  gap: "1rem",
+  width: "100%",
+});
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [storedTheme, setStoredTheme] = useState(
+    localStorage.getItem(LOCAL_STORAGE_THEME) ?? "system"
+  );
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:59458");
+
+    socket.onopen = () => {
+      console.log("WebSocket connection opened");
+    };
+
+    socket.onmessage = (event) => {
+      console.log("Received message:", event.data);
+    };
+
+    socket.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+
+    // Close the WebSocket connection when the component unmounts
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          secondary: {
+            main: "#00ff99",
+          },
+          mode:
+            storedTheme === "dark" || storedTheme === "light"
+              ? storedTheme
+              : prefersDarkMode
+              ? "dark"
+              : "light",
+        },
+      }),
+    [prefersDarkMode, storedTheme]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main" sx={{ mt: 10 }} maxWidth={false}>
+        <HomePage>
+          {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+          <Button>Test Hit Endpoint</Button>
+        </HomePage>
+      </Container>
+    </ThemeProvider>
   );
 }
 
